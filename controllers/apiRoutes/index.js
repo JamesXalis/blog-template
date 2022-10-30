7bf2ea74dd2439e07a49f69c41f164b46f219e8a
James Xalis <jamesxalis@icloud.com>
2022-10-29 23:57:34 -0700

    connected models and routes

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
-const {User, Post, Comment} = require("../../models")
 const {User, Post, Comment} = require("../../models");
 const userRoutes = require("./userRoutes");
 const blogPostRoutes = require("./blogPostRoutes")
+const commentRoutes = require("./commentRoutes");
 
-router.use("/users", userRoutes)
-router.use("/blogpost", blogPostRoutes)
+// router handlers for "/api" endpoints
+router.use("/user", userRoutes);
+router.use("/blogpost", blogPostRoutes);
+router.use("/comment", commentRoutes);
 
 module.exports = router;
